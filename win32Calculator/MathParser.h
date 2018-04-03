a78770ba7127cfd65ad86f7a8f66251b0e9c1ac7
ProfoundDisputes <whatsuhhguitar@gmail.com>
2018-04-03 10:08:56 -0400

    countNumbers() - Added prototype to header and changed extractAllNumbers() parameters.

--- win32Calculator/MathParser.h
+++ win32Calculator/MathParser.h
@@ -10,8 +10,9 @@
 class MATHPARSER
 {
 public:
-	bool extractAllNumbers(char* textToExtract, int* intArray, int maxIntArray) const;
+	bool extractAllNumbers(char* textToExtract, int* intArray, int textLength, int intLength) const;
 
+	int countNumbers(char* text, int textLength) const;
 private:
 
 };
