d12fc4b3ba26e4d98f632c784343df985fde7994
ProfoundDisputes <whatsuhhguitar@gmail.com>
2018-04-03 10:10:18 -0400

    extractAllNumbers() - Changed parameters to match header file.
    countNumbers() - Added some early implementation code

--- win32Calculator/MathParser.cpp
+++ win32Calculator/MathParser.cpp
@@ -6,7 +6,48 @@
 
 #include "MathParser.h"
 
-bool MATHPARSER::extractAllNumbers(char* textToExtract, int* intArray, int maxIntArray) const
+bool MATHPARSER::extractAllNumbers(char* textToExtract, int* intArray, int textLength, int intLength) const
 {
 
+	return true;
+}
+
+int MATHPARSER::countNumbers(char* text, int textLength) const
+{
+	//Stores the amount of number hits from 'text'
+	int count;
+
+	//First loop for searching array
+	for (int i = 0; i <= textLength; i++)
+	{
+		switch (text[i])
+		{
+
+		case '0':
+			break;
+		case '1':
+			break;
+		case '2':
+			break;
+		case '3':
+			break;
+		case '4':
+			break;
+		case '5':
+			break;
+		case '6':
+			break;
+		case '7':
+			break;
+		case '8':
+			break;
+		case '9':
+			break;
+
+		}
+
+
+	}
+
+	return count;
 }
