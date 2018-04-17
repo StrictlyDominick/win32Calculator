2f4bd137916653c01c289e25733946e1d06427ad
ProfoundDisputes <whatsuhhguitar@gmail.com>
2018-04-17 14:02:16 -0400

    countNumbers() - Removed switch statement checking for char position for number

--- win32Calculator/MathParser.cpp
+++ win32Calculator/MathParser.cpp
@@ -16,38 +16,63 @@ int MATHPARSER::countNumbers(char* text, int textLength) const
 {
 	//Stores the amount of number hits from 'text'
 	int count;
+	bool isNum = false;
 
 	//First loop for searching array
-	for (int i = 0; i <= textLength; i++)
+	for (int i = 1; i <= textLength; i++)
 	{
-		switch (text[i])
+
+
+		if (isNum == false)
 		{
+			if (text[i - 1] == ' ')
+			{
 
-		case '0':
-			break;
-		case '1':
-			break;
-		case '2':
-			break;
-		case '3':
-			break;
-		case '4':
-			break;
-		case '5':
-			break;
-		case '6':
-			break;
-		case '7':
-			break;
-		case '8':
-			break;
-		case '9':
-			break;
-
+			}
 		}
-
-
 	}
 
 	return count;
+}
+
+bool MATHPARSER::isNum(char character) const
+{
+	switch (character])
+	{
+
+	case '0':
+		return true;
+		break;
+	case '1':
+		return true;
+		break;
+	case '2':
+		return true;
+		break;
+	case '3':
+		return true;
+		break;
+	case '4':
+		return true;
+		break;
+	case '5':
+		return true;
+		break;
+	case '6':
+		return true;
+		break;
+	case '7':
+		return true;
+		break;
+	case '8':
+		return true;
+		break;
+	case '9':
+		return true;
+		break;
+	default:
+		return false;
+		break;
+
+	}
 }
