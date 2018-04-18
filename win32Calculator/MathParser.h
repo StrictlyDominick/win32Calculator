d2590e3244ff1855f3466c1d907d7f49918e9f55
ProfoundDisputes <whatsuhhguitar@gmail.com>
2018-04-18 14:32:52 -0400

    countNumbers() - Added comments describing function. No one commenting yet

--- win32Calculator/MathParser.h
+++ win32Calculator/MathParser.h
@@ -12,6 +12,13 @@ class MATHPARSER
 public:
 	bool extractAllNumbers(char* textToExtract, int* intArray, int textLength, int intLength) const;
 
+	//Count numbers contained in char string in two different ways depending on optional param.
+	//If "delimited" is true it will count consecutive numbers as a single number. For example
+	//"121d32sde" will count as 2 numbers; without delimiter enabled "121d32sde" will count
+	//as 5 numbers. Once numbers have been counted it will return the tallied amount as integer.
+	//Postcondition:	Will return integer showing tallied amount of numbers contained in 
+	//					'text'. Return value will change depending on 'delimited' value.
+	//Precondition :	'textLength' must be the size of the array
 	int countNumbers(const char* text, int textLength, bool delimited = false) const;
 
 	bool isNum(char character) const;
