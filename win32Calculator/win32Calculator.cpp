4c3da40187c202428b92b405dae45c3394b45a6e
ProfoundDisputes <whatsuhhguitar@gmail.com>
2018-05-07 10:30:04 -0400

    Deleted Task from TODO section.

--- win32Calculator/win32Calculator.cpp
+++ win32Calculator/win32Calculator.cpp
@@ -39,9 +39,6 @@
 		 ID_EDIT can't exceed the size of its container. Also allows 
 		 assumptions on the maximum size of the string contained in
 		 the text field.
-		-4-22-2018 7:11pm
-		 extractAllNumbers() function within MathPaser.cpp needs to be
-		 updated to include grabbing numbers even when they are decimals
 
 **********************************************************************/
 
